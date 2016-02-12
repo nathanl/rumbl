c59910c1b1c63ab3c62ea0a10e9ea19748116660
Nathan Long <him@nathanmlong.com>
2016-02-12 14:31:40 -0500

    Now creating users

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,5 +1,7 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
+  @required_params ~w(name username)
+  @optional_params ~w()
 
   schema "users" do
     field :name, :string
@@ -8,5 +10,11 @@ defmodule Rumbl.User do
     field :password_hash, :string
 
     timestamps
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_params, @optional_params)
+    |> validate_length(:username, min: 1, max: 20)
   end
 end
